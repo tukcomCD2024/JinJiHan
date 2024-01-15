1f35c3bc77c951ecaa96f1a5be3b5dab43e15003
realhsb <realhsb@gmail.com>
2024-01-15 23:50:22 +0900

    🎉 파일 생성
    iOS 17.0  SwiftUI 생성

---
+++ iOS/RollTheDice/RollTheDice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RollTheDice
+//
+//  Created by Subeen on 1/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS/RollTheDice/RollTheDice/RollTheDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  RollTheDiceApp.swift
+//  RollTheDice
+//
+//  Created by Subeen on 1/15/24.
+//
+
+import SwiftUI
+
+@main
+struct RollTheDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
