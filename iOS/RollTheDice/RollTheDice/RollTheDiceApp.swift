449b394d34afd301315bd8e9ed5723c7cd585d45
realhsb <realhsb@gmail.com>
2024-03-10 19:37:27 +0900

    feat: NavigationStack을 위한 Path 설정

--- iOS/RollTheDice/RollTheDice/RollTheDiceApp.swift
+++ iOS/RollTheDice/RollTheDice/RollTheDiceApp.swift
@@ -9,9 +9,27 @@ import SwiftUI
 
 @main
 struct RollTheDiceApp: App {
+    
+    @StateObject var appState = AppState()
+    @StateObject private var pathModel: PathModel = .init(paths: [.mainTabView])
+    
     var body: some Scene {
         WindowGroup {
-            MainTabView()
+            
+            if appState.hasLogin {
+                NavigationStack(path: $pathModel.paths) {
+                    MainTabView()
+                        .ig
+                }
+            } else {
+                SignUpView()
+            }
         }
     }
+}
+
+/// 로그인 상태 관리
+class AppState: ObservableObject {
+    
+    @AppStorage("hasLogin") var hasLogin = true
 }
---
+++ iOS/RollTheDice/RollTheDice/Source/Model/Path/PathType.swift
@@ -0,0 +1,13 @@
+//
+//  PathType.swift
+//  RollTheDice
+//
+//  Created by Subeen on 3/10/24.
+//
+
+import Foundation
+
+enum PathType: Hashable {
+    case mainTabView
+    case chatView
+}
