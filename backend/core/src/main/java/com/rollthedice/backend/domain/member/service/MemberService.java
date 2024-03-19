d5997ee37949c7d4571705ca7ed643b1d070aa8c
yeonjy <yeonjypaper@gmail.com>
2024-03-19 18:06:12 +0900

    feat: @Transactional 추가

--- backend/core/src/main/java/com/rollthedice/backend/domain/member/service/MemberService.java
+++ backend/core/src/main/java/com/rollthedice/backend/domain/member/service/MemberService.java
@@ -5,12 +5,14 @@ import com.rollthedice.backend.domain.member.entity.Member;
 import com.rollthedice.backend.domain.member.query.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class MemberService {
     private final AuthService authService;
 
+    @Transactional
     public void signUp(SignUpDto dto) {
         Member member = authService.getMember();
         member.updateNickname(dto.getNickname());
