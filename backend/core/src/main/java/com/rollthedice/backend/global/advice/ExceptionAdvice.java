5e467b7f8ec5b8475f8651099c5e28f66ed89221
yeonjy <yeonjypaper@gmail.com>
2024-03-26 04:49:22 +0900

    feat: ExceptionAdvice 추가

---
+++ backend/core/src/main/java/com/rollthedice/backend/global/advice/ExceptionAdvice.java
@@ -0,0 +1,16 @@
+package com.rollthedice.backend.global.advice;
+
+import com.rollthedice.backend.global.jwt.exception.NotFoundTokenException;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExceptionAdvice {
+    @ExceptionHandler(NotFoundTokenException.class)
+    public ResponseEntity<HttpEntity> notFoundTokenException() {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+}
