5cca59d5cdf58f2b2ee227184f2547f87e6283cd
realhsb <realhsb@gmail.com>
2024-04-22 00:45:21 +0900

    chore: 통계 뷰를 위한 내비게이션 path type 추가

--- iOS/RollTheDice/RollTheDice/Source/Model/Path/PathType.swift
+++ iOS/RollTheDice/RollTheDice/Source/Model/Path/PathType.swift
@@ -9,4 +9,7 @@ import Foundation
 
 enum PathType: Hashable {
     case chatView(isAiMode: Bool)
+    case detailNewsView
+    case typeReportView     // 분야별 뉴스 통계
+    case dailyReportView    // 요일별 뉴스 관람 개수 통계
 }
