8a2e0f72fd9712bd9d82a9464ad673155d26afeb
yeonjy <yeonjypaper@gmail.com>
2024-04-22 16:12:03 +0900

    feat: DebateMessage Entity 구현

---
+++ backend/core/src/main/java/com/rollthedice/backend/domain/debate/entity/DebateMessage.java
@@ -0,0 +1,39 @@
+package com.rollthedice.backend.domain.debate.entity;
+
+import com.rollthedice.backend.domain.member.entity.Member;
+import com.rollthedice.backend.global.config.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class DebateMessage extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String message;
+
+    @Enumerated(EnumType.STRING)
+    private SenderType senderType;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sender_id")
+    private Member sender;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "debate_room_id")
+    private DebateRoom debateRoom;
+
+    @Builder
+    public DebateMessage(String message, SenderType senderType, DebateRoom debateRoom) {
+        this.message = message;
+        this.senderType = senderType;
+        this.debateRoom = debateRoom;
+    }
+
+}
