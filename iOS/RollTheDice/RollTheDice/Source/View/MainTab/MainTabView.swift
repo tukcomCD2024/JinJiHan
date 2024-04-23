daf0cc39a26491ca7884a5aff4719e0a75df2876
realhsb <realhsb@gmail.com>
2024-04-23 17:17:10 +0900

    chore: 메인탭뷰 커스텀 내비게이션 연결

--- iOS/RollTheDice/RollTheDice/Source/View/MainTab/MainTabView.swift
+++ iOS/RollTheDice/RollTheDice/Source/View/MainTab/MainTabView.swift
@@ -18,30 +18,39 @@ struct MainTabView: View {
         ZStack {
             Color.backgroundDark
                 .ignoresSafeArea(.all)
-            
-            TabView(selection: $mainTabViewModel.selectedTabItem) {
+            VStack {
+                CustomNavigationBar(
+                    isDisplayTrailingBtn: true,
+                    trailingItems: [
+                        (Image(.bookmarkfill), { pathModel.paths.append(.bookmarkView)}),
+                        (Image(.profileWhite), { pathModel.paths.append(.mypageView)})
+                    ]
+                )
                 
-                ReportListView()
-                    .tabItem {
-                        Image(systemName: "list.bullet.rectangle")
-                    }
-                    .tag(0)
-                
-                NewsListView(newsListViewModel: newsListViewModel)
-                    .tabItem {
-                        Image(systemName: "square.3.layers.3d.down.left")
-                    }
-//                    .environmentObject(newsListViewModel)
-                    .tag(1)
-                
-                ChatTypeView()
-//                    .environmentObject(pathModel)
-                    .tabItem {
-                        Image(systemName: "message")
-                    }
-//                    .environmentObject(pathModel)
-                    .tag(2)
-
+                TabView(selection: $mainTabViewModel.selectedTabItem) {
+                    
+                    ReportListView()
+                        .tabItem {
+                            Image(systemName: "list.bullet.rectangle")
+                        }
+                        .tag(0)
+                    
+                    NewsListView(newsListViewModel: newsListViewModel)
+                        .tabItem {
+                            Image(systemName: "square.3.layers.3d.down.left")
+                        }
+                    //                    .environmentObject(newsListViewModel)
+                        .tag(1)
+                    
+                    ChatTypeView()
+                    //                    .environmentObject(pathModel)
+                        .tabItem {
+                            Image(systemName: "message")
+                        }
+                    //                    .environmentObject(pathModel)
+                        .tag(2)
+                    
+                }
             }
         }
     }
