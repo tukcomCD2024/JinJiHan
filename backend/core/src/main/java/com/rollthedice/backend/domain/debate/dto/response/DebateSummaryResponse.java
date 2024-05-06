845d337b06b4ca0a981083d6d71df26411232f83
yeonjy <yeonjypaper@gmail.com>
2024-05-06 23:40:38 +0900

    feat: response dto 설명 추가

--- backend/core/src/main/java/com/rollthedice/backend/domain/debate/dto/response/DebateSummaryResponse.java
+++ backend/core/src/main/java/com/rollthedice/backend/domain/debate/dto/response/DebateSummaryResponse.java
@@ -1,5 +1,6 @@
 package com.rollthedice.backend.domain.debate.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,5 +12,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class DebateSummaryResponse {
     Long roomId;
+
+    @Schema(description = "4문장의 요약된 토론 내용")
     String summary;
 }
