73543434bbe07f1e90bd789eb5b1af77297ffa12
realhsb <realhsb@gmail.com>
2024-05-16 00:35:46 +0900

    chore: 카드 뉴스 및 뉴스 목록 UI 수정

--- iOS/RollTheDice/RollTheDice/Source/View/News/NewsCard/NewsListView.swift
+++ iOS/RollTheDice/RollTheDice/Source/View/News/NewsCard/NewsListView.swift
@@ -11,23 +11,17 @@ struct NewsListView: View {
     
     var newsListViewModel: NewsListViewModel
     @State var selectedIndex: Int = 0
+
     
     var body: some View {
         ZStack {
             Color.backgroundDark.ignoresSafeArea(.all)
-            NewsListContentView(newsList: newsListViewModel.newsList)
-                .padding(.leading, 20)
-        }
-    }
-    
-    private struct NewsListContentView: View {
-        var newsList: [News]
-        
-        fileprivate var body: some View {
+            
+            
             GeometryReader { proxy in
                 ScrollView(.horizontal, showsIndicators: false) {
                     LazyHStack {
-                        ForEach(newsList , id: \.self) { news in
+                        ForEach(newsListViewModel.newsList ?? []) { news in
                             
                             NewsView(news: news)
                                 .frame(width: proxy.size.width)
@@ -35,7 +29,7 @@ struct NewsListView: View {
                                     effect
                                         .scaleEffect(phase.isIdentity ? 1 : 0.8)
                                         .blur(radius: phase.isIdentity ? 0 : 1)
-                                        .offset(x: offset(for: phase))
+//                                        .offset(x: offset(for: phase))
                                 }
                         }
                     }
@@ -43,19 +37,64 @@ struct NewsListView: View {
                 }
                 .scrollTargetBehavior(.viewAligned)
             }
+//            NewsListContentView(newsList: $newsListViewModel.newsList)
+//                .padding(.leading, 20)
         }
-        
-        func offset (for phase: ScrollTransitionPhase) -> Double {
-            switch phase {
-            case .topLeading:
-                700
-            case .identity:
-                0
-            case .bottomTrailing:
-                -700
-            }
+        .task {
+            newsListViewModel.getAllNewsData(page: 0, size: 10)
         }
     }
+    
+    func offset (for phase: ScrollTransitionPhase) -> Double {
+        switch phase {
+        case .topLeading:
+            700
+        case .identity:
+            0
+        case .bottomTrailing:
+            -700
+        }
+    }
+    
+//    private struct NewsListContentView: View {
+//        @Binding var newsList: [News]
+//        
+//       
+//        
+//        fileprivate var body: some View {
+//            GeometryReader { proxy in
+//                ScrollView(.horizontal, showsIndicators: false) {
+//                    LazyHStack {
+//                        ForEach(newsList , id: \.self) { news in
+//                            
+//                            NewsView(news: news)
+//                                .frame(width: proxy.size.width)
+//                                .scrollTransition(.interactive, axis: .horizontal) { effect, phase in
+//                                    effect
+//                                        .scaleEffect(phase.isIdentity ? 1 : 0.8)
+//                                        .blur(radius: phase.isIdentity ? 0 : 1)
+//                                        .offset(x: offset(for: phase))
+//                                }
+//                        }
+//                    }
+//                    .scrollTargetLayout()
+//                }
+//                .scrollTargetBehavior(.viewAligned)
+//            }
+            
+//        }
+        
+//        func offset (for phase: ScrollTransitionPhase) -> Double {
+//            switch phase {
+//            case .topLeading:
+//                700
+//            case .identity:
+//                0
+//            case .bottomTrailing:
+//                -700
+//            }
+//        }
+//    }
 }
 
 #Preview {
