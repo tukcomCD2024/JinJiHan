e0818f3ce83cafbfbc818f67aa10fb8f29957e5b
yeonjy <yeonjypaper@gmail.com>
2024-06-17 08:17:36 +0900

    test: Database 관련 test 설정

---
+++ backend/core/src/test/java/com/rollthedice/backend/support/database/DatabaseTest.java
@@ -0,0 +1,17 @@
+package com.rollthedice.backend.support.database;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Transactional
+@ActiveProfiles("test")
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+public @interface DatabaseTest {}
