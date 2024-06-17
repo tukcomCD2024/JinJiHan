c3427ec9ad710624a07858df09a66f2a659f1188
yeonjy <yeonjypaper@gmail.com>
2024-06-17 21:48:51 +0900

    test: StatisticsServiceTest 구현

---
+++ backend/core/src/test/java/com/rollthedice/backend/domain/statistics/service/StatisticsServiceTest.java
@@ -0,0 +1,41 @@
+package com.rollthedice.backend.domain.statistics.service;
+
+import com.rollthedice.backend.domain.news.repository.ReadNewsRepository;
+import com.rollthedice.backend.domain.statistics.dto.DateViewStatisticsResponse;
+import com.rollthedice.backend.global.LoginTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+
+@DisplayName("StatisticsService의 ")
+@ExtendWith(MockitoExtension.class)
+public class StatisticsServiceTest extends LoginTest {
+    @Autowired
+    private StatisticsService statisticsService;
+    @MockBean
+    private ReadNewsRepository readNewsRepository;
+
+    @Test
+    @DisplayName("최근 일주일 날짜별 뉴스 조회수가 조회되는가")
+    void getViewsOfDates() {
+        //given
+        int oneWeek = 7;
+        given(readNewsRepository.findReadNewsByDate(LocalDate.now())).willReturn(anyLong());
+
+        //when
+        List<DateViewStatisticsResponse> result = statisticsService.getViewsOfDates();
+
+        //then
+        assertThat(result).hasSize(oneWeek);
+    }
+}
