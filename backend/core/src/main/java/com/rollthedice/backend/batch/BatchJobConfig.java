5d75a47044d0799fdd813831e88eb096b5c9e908
yeonjy <yeonjypaper@gmail.com>
2024-07-21 00:32:32 +0900

    feat: BathJobConfig 구현
    추후 Step들을 별도의 클래스로 나누어야 함

--- backend/core/src/main/java/com/rollthedice/backend/batch/BatchJobConfig.java
+++ backend/core/src/main/java/com/rollthedice/backend/batch/BatchJobConfig.java
@@ -1,25 +1,29 @@
 package com.rollthedice.backend.batch;
 
+import com.rollthedice.backend.batch.newsContentStep.PreSummarizedNewsDto;
+import com.rollthedice.backend.batch.newsContentStep.UncrawledNewsContentReader;
 import com.rollthedice.backend.batch.newsUrlStep.InitNewsDto;
 import com.rollthedice.backend.batch.newsUrlStep.NewsUrlReader;
+import com.rollthedice.backend.domain.news.contentqueue.ContentProducer;
+import com.rollthedice.backend.domain.news.dto.ContentMessageDto;
+import com.rollthedice.backend.domain.news.repository.NewsRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepScope;
-import org.springframework.batch.core.configuration.support.DefaultBatchConfiguration;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
@@ -28,16 +32,21 @@ import javax.sql.DataSource;
 @Configuration
 @RequiredArgsConstructor
 public class BatchJobConfig {
-    private final DataSource dataSource;
 
+    @Value("${batch.chunk-size}")
+    private int chunkSize;
+
+    private final DataSource dataSource;
+    private final NewsRepository newsRepository;
+    private final ContentProducer contentProducer;
 
     @Bean
     public Job scrapJob(JobRepository jobRepository,
-                        Step crawlingNewsUrlStep) {
+                        Step crawlingNewsUrlStep, Step crawlingNewsContentStep) {
         return new JobBuilder("scrapJob", jobRepository)
                 .incrementer(new RunIdIncrementer())
                 .start(crawlingNewsUrlStep)
-//                .next(crawlingNewsContentStep)
+                .next(crawlingNewsContentStep)
                 .build();
     }
 
@@ -62,6 +71,45 @@ public class BatchJobConfig {
         return new JdbcBatchItemWriterBuilder<InitNewsDto>()
                 .dataSource(dataSource)
                 .sql("insert into news(url, category) values (:url, :newsCategory)")
+                .beanMapped()
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step crawlingNewsContentStep(JobRepository jobRepository,
+                                        PlatformTransactionManager transactionManager) {
+        return new StepBuilder("crawlingNewsContentStep", jobRepository)
+                .allowStartIfComplete(true)
+                .<PreSummarizedNewsDto, PreSummarizedNewsDto>chunk(chunkSize, transactionManager)
+                .reader(uncrawledNewsContentReader())
+                .processor(summarizeContentProcessor())
+                .writer(newsContentWriter())
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    public ItemReader<PreSummarizedNewsDto> uncrawledNewsContentReader() {
+        return new UncrawledNewsContentReader(newsRepository);
+    }
+
+    @Bean
+    @StepScope
+    public ItemProcessor<PreSummarizedNewsDto, PreSummarizedNewsDto> summarizeContentProcessor() {
+        return dto -> {
+            contentProducer.sendMessage(new ContentMessageDto(dto.getId(), dto.getContent()));
+            return dto;
+        };
+    }
+
+    @Bean
+    @StepScope
+    public JdbcBatchItemWriter<PreSummarizedNewsDto> newsContentWriter() {
+        return new JdbcBatchItemWriterBuilder<PreSummarizedNewsDto>()
+                .dataSource(dataSource)
+                .sql("update news set title = :title, content = :content, post_date = :postDate" +
+                        " where id = :id")
                 .beanMapped()
                 .build();
     }
