b3323412c32cc1a6afb14b178fdcbc3a496fb145
realhsb <realhsb@gmail.com>
2024-09-05 16:17:43 +0900

    데모용 프로젝트

---
+++ iOS/Scoop/Scoop/ScoopApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoopApp.swift
+//  Scoop
+//
+//  Created by Subeen on 9/5/24.
+//
+
+import SwiftUI
+
+@main
+struct ScoopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
