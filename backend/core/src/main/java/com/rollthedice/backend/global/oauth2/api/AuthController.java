05d739d06047e5d0fd30467a9ccb599878024146
yeonjy <yeonjypaper@gmail.com>
2024-09-18 20:56:01 +0900

    !Hotfix: login 응답 충돌 해결

--- backend/core/src/main/java/com/rollthedice/backend/global/oauth2/api/AuthController.java
+++ backend/core/src/main/java/com/rollthedice/backend/global/oauth2/api/AuthController.java
@@ -10,13 +10,11 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import static com.rollthedice.backend.global.common.response.SuccessCode.GET_SOCIAL_LOGIN_SUCCESS;
 import static com.rollthedice.backend.global.common.response.SuccessCode.UPDATE_NICKNAME_SUCCESS;
 
 @Slf4j
@@ -29,9 +27,8 @@ public class AuthController implements AuthApi {
     @ResponseStatus(HttpStatus.OK)
     @PostMapping("/login")
     @Override
-    public SuccessResponse<String> login(@RequestBody LoginRequest request, HttpServletResponse response) {
+    public void login(@RequestBody LoginRequest request, HttpServletResponse response) {
         authService.authenticateOrRegisterUser(request, response);
-        return SuccessResponse.of(GET_SOCIAL_LOGIN_SUCCESS);
     }
 
     @PostMapping("/oauth2/sign-up")
